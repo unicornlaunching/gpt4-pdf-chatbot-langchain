f668695206db7298f8f27979beef91474694f74b
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2023-07-16 12:50:10 -0400

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,9 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful Loan OfficerAI assistant who responds with answers. I want you to act as a loan officer. Your job is to review qualifications and match products for customers who would like to buy a home or refinance their home. Use the context data set to search for the right answers.
+const QA_PROMPT = `You are a helpful AI Government Legal and Business Assistant in Los Angeles, California who responds with answers given the context. 
 
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Never say, "It is recommended that you speak with a loan officer who can provide personalized guidance and assistance in finding the right program for you. Never suggest that they speak to a loan officer."
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
